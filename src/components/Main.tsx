cb4776f38e5fe3341b0b12e4c0e63e6dee4fc89a
Andrew <bakarekeandrew@gmail.com>
2025-07-25 11:17:28 +0200

    first commit

---
+++ src/components/Main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Todos from "./todos";
+
+const Main: React.FC = () => {
+  return (
+    <Todos />
+  )
+}
+
+export default Main
---
+++ src/components/Todos.tsx
@@ -0,0 +1,35 @@
+import React, {useState} from 'react'
+
+const Todos: React.FC = () => {
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<string[]>([]);
+
+     
+   const handleEvent = (e) => {
+     setTodo(e.target.value);
+   }
+   const handleAddButton = () => {
+     setTodos(prev => [...prev, todo]);
+     setTodo("");
+   }
+  
+  return (
+    
+    <div className='pt-10 p-10'>
+        <div className='flex gap-x-4'>
+            <input className='w-80 border-amber-600' value={todo} onChange={handleEvent}/> 
+            <div className='bg-green-400 rounded-xl'>
+                <button className='p-2' onClick={handleAddButton}>Add</button>       
+            </div> 
+        </div>
+        <ul>
+            {todos.map((item, index) =>
+             (<li key={index}>{item}</li>
+           ))}
+        </ul>
+    </div>
+    
+  )
+}
+
+export default Todos
